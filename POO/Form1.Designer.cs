53132211d48b4dd80a8de726c1828c3e0df4e056
Daniel <danielbarros854@yahoo.com>
2021-08-31 21:46:06 -0300

    Adicionar arquivos de projeto.

---
+++ POO/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace POO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.texto_1 = new System.Windows.Forms.TextBox();
+            this.texto_2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.funcao = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(418, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "&Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // texto_1
+            // 
+            this.texto_1.Location = new System.Drawing.Point(344, 148);
+            this.texto_1.Name = "texto_1";
+            this.texto_1.Size = new System.Drawing.Size(100, 20);
+            this.texto_1.TabIndex = 4;
+            this.texto_1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // texto_2
+            // 
+            this.texto_2.Location = new System.Drawing.Point(467, 148);
+            this.texto_2.Name = "texto_2";
+            this.texto_2.Size = new System.Drawing.Size(100, 20);
+            this.texto_2.TabIndex = 5;
+            this.texto_2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Calculadora";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Calculadora";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // funcao
+            // 
+            this.funcao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.funcao.FormattingEnabled = true;
+            this.funcao.Location = new System.Drawing.Point(395, 188);
+            this.funcao.Name = "funcao";
+            this.funcao.Size = new System.Drawing.Size(121, 21);
+            this.funcao.TabIndex = 7;
+            this.funcao.Tag = "funcao";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "1º Valor";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(495, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "2º Valor";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(392, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Tag = "resultado";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // sair
+            // 
+            this.sair.Location = new System.Drawing.Point(418, 333);
+            this.sair.Name = "sair";
+            this.sair.Size = new System.Drawing.Size(75, 23);
+            this.sair.TabIndex = 12;
+            this.sair.Text = "Sair";
+            this.sair.UseVisualStyleBackColor = true;
+            this.sair.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1456, 690);
+            this.Controls.Add(this.sair);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.funcao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.texto_2);
+            this.Controls.Add(this.texto_1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox texto_1;
+        private System.Windows.Forms.TextBox texto_2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox funcao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button sair;
+    }
+}
+
---
+++ POO/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resultado(double r)
+        {
+            label6.Text = $"Resultado: {string.Format("{0:N}", r)}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((texto_1.Text != "") && (texto_2.Text != ""))
+            {
+                double valor1 = Convert.ToDouble(texto_1.Text), valor2 = Convert.ToDouble(texto_2.Text), result = 0;
+                string tipo = Convert.ToString(funcao.SelectedItem);
+
+                switch (tipo)
+                {
+                    case "Somar":
+                        result = valor1 + valor2;
+                        resultado(result);
+                        break;
+
+                    case "Subtrair":
+                        result = valor1 - valor2;
+                        resultado(result);
+                        break;
+
+                    case "Multiplicar":
+                        result = valor1 * valor2;
+                        resultado(result);
+                        break;
+
+                    case "Dividir":
+                        result = valor1 / valor2;
+                        resultado(result);
+                        break;
+
+                    default:
+                        label6.Text = "";
+                        MessageBox.Show("Selecione uma função");
+                        break;
+                }
+
+            }
+            else
+            {
+
+                label6.Text = "";
+                MessageBox.Show("Ops!! Campo Vazio");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            funcao.Items.Add("Somar");
+            funcao.Items.Add("Subtrair");
+            funcao.Items.Add("Multiplicar");
+            funcao.Items.Add("Dividir");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(texto_1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Use apenas numeros.");
+                texto_1.Text = texto_1.Text.Remove(texto_1.Text.Length - 1);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(texto_2.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Use apenas numeros.");
+                texto_2.Text = texto_2.Text.Remove(texto_2.Text.Length - 1);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
